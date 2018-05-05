eb7e9b7c01c73bf4b5c759f34bdec6879f1d9b58
Nandadeep Davuluru <aditya.nandadeep@gmail.com>
2018-05-05 10:46:04 -0700

    fixed true/predicted labels, regularization, setup pruning

--- regression-tree.py
+++ regression-tree.py
@@ -1,17 +1,23 @@
 import pandas as pd 
-import numpy, sys
-
+import numpy, sys, tqdm
+from sklearn.metrics import mean_squared_error
 
 class Utils(object):
 
 	def __init__(self, path, targets):
 		self.dataset = pd.read_csv(filepath_or_buffer = path, header = 'infer')
 		self.target = targets
+		self.columnlist = list(self.dataset)
+		self.min_rss = None
+		self.min_chunk = None
+		# enter hyperparameters
+		self.alpha = 0.25
 
 	def process (self, training_set_size):
 		# edge case -- 1
 		if training_set_size > 1:
 			training_set_size = training_set_size / 100
+
 		# randomly shuffle all rows
 		self.dataset = self.dataset.sample(frac = 1)
 		# split train : test :: training_set_size : 1 - training_set_size ratio
@@ -20,6 +26,8 @@ class Utils(object):
 		return train_dataset, test_data
 
 	computeTarget = lambda self, chunk : chunk[self.target].median()
+	regularize = lambda self, data : self.computeRSS(data) + (self.alpha * len(data.columns.tolist()))
+
 
 	def performSplit (self, train_data, feature):
 		# apply split function near mean
@@ -30,30 +38,35 @@ class Utils(object):
 	def computeRSS (self, chunk):
 		# (true of each entry in chunk - median of chunk) ** 2
 		rss = 0.0
-		true_target = self.computeTarget(chunk)
-		for index, row in chunk.iterrows():
-			rss += numpy.power((row[self.target] - true_target), 2)
+		predicted_target = self.computeTarget(chunk)
+		for index, row in tqdm.tqdm(chunk.iterrows()):
+			rss += numpy.power((row[self.target] - predicted_target), 2)
 		return rss 
 
-	def recursiveSplit (self, train_data):
+	def displayContents (self):
+		print(self.min_rss, self.min_chunk)
 
+
+	def recursiveSplit (self, train_data, count):
+
+		if len(train_data) < (0.025 * len(self.dataset)):
+			return 0
+
+		self.displayContents()
 		# generate columns based on the best rss value
-		columns = ["CompPrice",  "Income",  "Advertising",  "Population",  "Price",  "Age"]
+		columns = ["CompPrice",  "Income",  "Advertising",  "Population",  "Price",  "Age", "Education"]
 		chunks = list()
 		rssValues = list()
-		for column in columns:
+		for column in tqdm.tqdm(columns):
 			t1, t2 = self.performSplit(train_data, column)
 			rssValues.extend((self.computeRSS(t1), self.computeRSS(t2)))
 			chunks.extend((t1, t2))
+		# find min rss 
+		self.min_rss = numpy.argmin(rssValues)
+		self.min_chunk = chunks[self.min_rss]
 
-		# print(min(rssValues), rssValues)
+		return self.recursiveSplit (self.min_chunk, count)
 		
-
-
-
-
-
-
 
 
 def main ():
@@ -65,11 +78,7 @@ def main ():
 	util = Utils(DATASET, 'Sales')
 	train, test = util.process(0.8)
 
-	# t, u = util.performSplit(train, 'Price')
-	# print(util.computeRSS(t), util.computeRSS(u))
-	# print(util.computeTarget(t))
-
-	util.recursiveSplit(train)
+	util.recursiveSplit(train, 0)
 
 		
 if __name__ == '__main__':
